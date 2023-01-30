4b3a8d4c751245196d3f2a6a53d6024f3fd2fe32
Sumit Tripathi <sumit.tpt07@gmail.com>
2023-01-30 16:17:00 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <>
       <div className="app">
-        <h1>MovieLand</h1>
+        <h1>MovieWorld</h1>
         <div className="search">
           <input
             type="text"
